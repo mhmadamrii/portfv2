c415d9f7cbb67bcdc09a2ca5f8aaa53da69715fe
mhmadamrii <interceptorghost4@gmail.com>
2024-11-02 01:42:26 +0700

    lfadk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,169 @@
+'use client';
+
 import Image from 'next/image';
-import Link from 'next/link';
+import GridLayout from 'react-grid-layout';
 
-export default function Home() {
+import { FloatingDock } from '~/components/ui/floating-dock';
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from '@tabler/icons-react';
+
+const links = [
+  {
+    title: 'Home',
+    icon: (
+      <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#',
+  },
+
+  {
+    title: 'Products',
+    icon: (
+      <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#',
+  },
+  {
+    title: 'Components',
+    icon: (
+      <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#',
+  },
+  {
+    title: 'Aceternity UI',
+    icon: (
+      <Image
+        src="https://assets.aceternity.com/logo-dark.png"
+        width={20}
+        height={20}
+        alt="Aceternity Logo"
+      />
+    ),
+    href: '#',
+  },
+  {
+    title: 'Changelog',
+    icon: (
+      <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#',
+  },
+
+  {
+    title: 'Twitter',
+    icon: (
+      <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#',
+  },
+  {
+    title: 'GitHub',
+    icon: (
+      <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#',
+  },
+];
+
+const layout = [
+  {
+    w: 2,
+    h: 7,
+    x: 0,
+    y: 0,
+    i: '1',
+  },
+  {
+    w: 4,
+    h: 2,
+    x: 2,
+    y: 0,
+    i: '2',
+  },
+  {
+    w: 2,
+    h: 5,
+    x: 2,
+    y: 2,
+    i: '3',
+  },
+  {
+    w: 2,
+    h: 2,
+    x: 4,
+    y: 2,
+    i: '4',
+  },
+  {
+    w: 2,
+    h: 3,
+    x: 4,
+    y: 4,
+    i: '5',
+  },
+  {
+    w: 2,
+    h: 4,
+    x: 0,
+    y: 7,
+    i: '6',
+  },
+];
+
+export default function Root() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Link href="/playground">Playground</Link>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className="flex flex-col justify-center items-center h-screen">
+      <div className="w-1/2 flex z-50 fixed bottom-[40px] justify-center">
+        <FloatingDock mobileClassName="translate-y-20" items={links} />
+      </div>
+      <div className="w-full flex items-center justify-center h-full border border-red-500 relative z-30 noselect">
+        <GridLayout
+          className="layout"
+          layout={layout}
+          cols={6}
+          rowHeight={40}
+          width={1400}
+          isResizable={true}
+          isDraggable={true}
+          onLayoutChange={layout => console.log(layout)}
+          style={{
+            border: '1px solid red',
+            width: '90%',
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <div key="1" className="cursor-pointer bg-red-400 rounded-lg px-5">
+            Item 1
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              tempore deleniti assumenda? Ut dolor aliquam eius corrupti.
+              Dolorum consectetur a earum nulla eaque nisi, blanditiis assumenda
+              ut similique in.
+            </p>
+          </div>
+          <div key="2" style={{ background: '#FF4500', color: 'white' }}>
+            Item 2
+          </div>
+          <div key="3" style={{ background: '#32CD32', color: 'white' }}>
+            Item 3
+          </div>
+          <div key="4" style={{ background: '#FFD700', color: 'black' }}>
+            Item 4
+          </div>
+          <div key="5" style={{ background: '#FF69B4', color: 'white' }}>
+            Item 5
+          </div>
+          <div key="6" style={{ background: '#8A2BE2', color: 'white' }}>
+            Item 6
+          </div>
+        </GridLayout>
+      </div>
+    </section>
   );
 }
--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Suspense } from 'react';
+import FlickeringGrid from '~/components/ui/flickering-grid';
 
 export default async function Playground({
   searchParams,
@@ -36,6 +37,16 @@ export async function PlaygroundWithData() {
           <p>{item.title}</p>
         </div>
       ))}
+      <FlickeringGrid
+        className="z-0 absolute inset-0 size-full"
+        squareSize={4}
+        gridGap={6}
+        color="#6B7280"
+        maxOpacity={0.5}
+        flickerChance={0.1}
+        height={800}
+        width={800}
+      />
     </section>
   );
 }
